1de0b8ad6776370227ad6a4e71e8f3140d49b94b
Malek <malekyasser10@gmail.com>
2023-08-23 23:06:10 +0300

    Rating Service Screen finished

--- lib/features/rateService_view/presentation/views/rateService_view.dart
+++ lib/features/rateService_view/presentation/views/rateService_view.dart
@@ -1,10 +1,12 @@
+import 'package:doctor_management_system/core/RepeatedWidgets/ButtonWidget.dart';
 import 'package:doctor_management_system/core/utils/Colors.dart';
 import 'package:doctor_management_system/core/utils/Constants.dart';
+import 'package:doctor_management_system/features/home_view/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
 
-import 'SelectedStarsUpdateUI.dart';
 
 class RateSeviceView extends StatefulWidget {
   const RateSeviceView({Key? key}) : super(key: key);
@@ -15,11 +17,18 @@ class RateSeviceView extends StatefulWidget {
 
 class _RateSeviceViewState extends State<RateSeviceView> {
   String dropdownValue1 = clinicName.first ;
+
+  final BasicController = TextEditingController();
+
   @override
+  void initState() {
+    super.initState();
+  }
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
           child: Padding(
             padding:  EdgeInsets.symmetric(horizontal : 6.w),
@@ -49,7 +58,10 @@ class _RateSeviceViewState extends State<RateSeviceView> {
                                 )
                               ]
                           ),
-                          child: Icon(Icons.arrow_forward_ios,color: Colors.black,)),
+                          child: Center(child: Padding(
+                            padding:  EdgeInsets.only(right: 2.w),
+                            child: Icon(Icons.arrow_back_ios,color: Colors.black,),
+                          ))),
                     ),
                     SizedBox(width: 4.w,),
                     Text("تقييم الخدمة",style: GoogleFonts.cairo(color: Colors.black,fontSize: 3.h,fontWeight: FontWeight.bold)),
@@ -96,33 +108,58 @@ class _RateSeviceViewState extends State<RateSeviceView> {
                   ),
                 ),
                 SizedBox(height: 2.h),
-            Container(
+                Container(
               color: AppColors.lightBlue.withOpacity(0.1),
               width: double.infinity,
               height: 8.h,
-              child: Padding(
-                padding: EdgeInsets.only(right: 9.w),
-                child: ListView.separated(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) => GestureDetector(
-                    onTap: () {
+              child: Center(
+                child: RatingBar(
+                  initialRating: 3,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 1.w),
+                  itemSize: 6.h,
+                  textDirection: TextDirection.ltr,
+                  allowHalfRating: false,
+                    minRating: 1,
+                    maxRating: 5,
+                    ratingWidget: RatingWidget(full: Icon(Icons.star_rounded,color: AppColors.golden), half:Icon(Icons.star_rounded,color: AppColors.golden) , empty: Icon(Icons.star_outline_rounded,color: Colors.grey,)),
+                    onRatingUpdate:(rating) {
                       setState(() {
-                        updateSelectedStars(index);
                       });
-                    },
-                    child:selectedStars[index] ? Icon(
-                      Icons.star_outline_rounded,
-                      size: 5.h,
-                    ) : Icon(
-                        Icons.star_rounded,
-                        size: 5.h,
-                      color: AppColors.golden,
+                    }, ),
+              )),
+                SizedBox(height: 2.h,),
+                Text("تفاصيل التقييم" , style: GoogleFonts.cairo(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 2.1.h),),
+                SizedBox(height: 1.h,),
+                TextField(
+                  textAlignVertical: TextAlignVertical.top,
+                  textAlign: TextAlign.start,
+                  textDirection: TextDirection.rtl,
+                  onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
+                  controller: BasicController,
+                  decoration: InputDecoration(
+                    enabledBorder:  OutlineInputBorder(
+                      borderSide:  BorderSide(color: Colors.grey.shade300, ),
+                    ),
+                    contentPadding: EdgeInsets.only(bottom: 18.h,right: 2.h,top: 1.h),
+                    isDense: true,
+
+                    hintText: "اضف تفاصيل اخرى لتقييمك اذا اردت..",
+                    hintStyle: GoogleFonts.cairo(color:Colors.grey.shade600 ),
+                    alignLabelWithHint: true,
+
                   ),
+                  keyboardType: TextInputType.emailAddress,
+
                 ),
-                  separatorBuilder: (context, index) => SizedBox(width: 5.w),
-                ),
-            ))],
+                Spacer(),
+                Padding(
+                  padding:  EdgeInsets.symmetric(vertical: 1.h),
+                  child: BasicButton(route: (context) => HomeView(),
+                      color: AppColors.lightBlue, Textcolor: Colors.white,
+                      TextSt: "تأكيد", borderColor: Colors.transparent),
+                )
+
+              ],
             ),
           ),
         ),
