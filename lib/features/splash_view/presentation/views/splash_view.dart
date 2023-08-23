aa4ea67e9f653a9c7d78d001bfb6150384bc8807
Rana <ra01210772310@gmail.com>
2023-08-23 19:20:27 +0300

    animatedSplashScreen

--- lib/features/splash_view/presentation/views/splash_view.dart
+++ lib/features/splash_view/presentation/views/splash_view.dart
@@ -1,12 +1,12 @@
+import 'package:doctor_management_system/features/splash_view/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
-
 class SplashView extends StatelessWidget {
   const SplashView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Splash View")),
+    return  const Scaffold(
+      body: SplashViewBody()
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:doctor_management_system/features/home_view/presentation/views/home_view.dart';
-import 'package:doctor_management_system/features/onboarding_view/presentation/views/onboarding_view.dart';
+import 'package:doctor_management_system/features/splash_view/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
@@ -16,7 +15,7 @@ class DoctorManagementSystem extends StatelessWidget {
         return  const MaterialApp(
           debugShowCheckedModeBanner: false,
           home: Scaffold(
-            body: OnboardingView(),
+            body: SplashView(),
           ),
         );
       },
