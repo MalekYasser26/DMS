65206830454efad7d20896e450357ca792bd0c5e
Malek <malekyasser10@gmail.com>
2023-08-29 03:16:18 +0300

    Finished patients list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:doctor_management_system/features/doctor_login/presentation/views/clinic.dart';
 import 'package:doctor_management_system/features/doctor_login/presentation/views/doctor_login_view.dart';
-import 'package:doctor_management_system/features/patientList/presentation/views/patient_list_view.dart';
-import 'package:doctor_management_system/features/splash_view/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
