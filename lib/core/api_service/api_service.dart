589ba8956cf0e1517e3527e4af7dfe2ed9be752d
Malek <malekyasser10@gmail.com>
2023-09-11 15:14:46 +0300

    gql fixing

--- lib/core/api_service/api_service.dart
+++ lib/core/api_service/api_service.dart
@@ -1,17 +1,21 @@
+import 'package:doctor_management_system/core/services/api/ucs_scheme.graphql.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
 
-class APIService {
-  // final GraphQLClient qlClient = GraphQLClient(
-  //   link: HttpLink('https://rasha-clinic.offer4us.com/graphql'),
-  //   cache: GraphQLCache(),
-  // );
 
-  // Future<String> getUserToken(
-  //     {required Input$UserCredentialsInput userCredentialsInput}) async {
-  //   final mutation =Options
-  //   final result = await qlClient.mutate(mutation) ;
-  // }
+class APIService {
+  final GraphQLClient qlClient = GraphQLClient(
+    link: HttpLink('https://rasha-clinic.offer4us.com/graphql'),
+    cache: GraphQLCache(),
+  );
+
+  Future<String> getUserToken(
+      {required Input$UserCredentialsInput userCredentialsInput}) async {
+    //final mutation = Options$Mutate
+   // final result = await qlClient.mutate(mutation) ;
+    return "";
+  }
 }
 
 APIService getAPIService(BuildContext context) =>
