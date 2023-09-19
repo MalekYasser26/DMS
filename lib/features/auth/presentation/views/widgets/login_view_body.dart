5dca34338b1423d85292693cb7d2f16ad9dba5ce
kerolosragaie <kerolosragaie@gmail.com>
2023-09-19 16:20:03 +0300

    adding SingleChildScrollView to LoginViewBody

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -18,137 +18,137 @@ class LoginViewBody extends StatelessWidget {
     final locale = getL10n(context);
     return SafeArea(
       child: Scaffold(
-        resizeToAvoidBottomInset: false,
         body: Padding(
-          padding:  EdgeInsets.all(6.w),
-          child: Column(
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      height: 4.h,
-                      width: 8.w,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(4),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(.3),
-                            blurRadius: 5.0,
-                            spreadRadius: 1,
-                            offset: const Offset(2.0, 2.0),
-                          )
-                        ],
-                      ),
-                      child: Padding(
-                        padding: !context.watch<PreferenceService>().isEn()
-                            ?  EdgeInsets.only(right: 2.w)
-                            :  EdgeInsets.only(left: 2.w),
-                        child: const Icon(
-                          Icons.arrow_back_ios,
-                          size: 16,
+          padding: EdgeInsets.all(6.w),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        height: 4.h,
+                        width: 8.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(.3),
+                              blurRadius: 5.0,
+                              spreadRadius: 1,
+                              offset: const Offset(2.0, 2.0),
+                            )
+                          ],
+                        ),
+                        child: Padding(
+                          padding: !context.watch<PreferenceService>().isEn()
+                              ? EdgeInsets.only(right: 2.w)
+                              : EdgeInsets.only(left: 2.w),
+                          child: const Icon(
+                            Icons.arrow_back_ios,
+                            size: 16,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(width: 3.w),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
+                    SizedBox(width: 3.w),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
                           locale.greeting,
-                        style: GoogleFonts.cairo(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: const Color(0xff171717)),
-                      ),
-                      Text(
-                        locale.fillData,
-                        style: GoogleFonts.cairo(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 14,
-                            color: const Color(0xff171717)),
-                      ),
-
-                    ],
-                  ),
-                ],
-              ),
-              SizedBox(height: 5.h),
-              BasicTextFormField(
-                text:locale.id,
-                isNumbers: true,
-              ),
-              BasicTextFormField(
-                text: locale.password,
-                isPass: true,
-              ),
-              SizedBox(height: 10.h),
-              InkWell(
-                onLongPress: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ClinicChoiceView(),
-                  ),
+                          style: GoogleFonts.cairo(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: const Color(0xff171717)),
+                        ),
+                        Text(
+                          locale.fillData,
+                          style: GoogleFonts.cairo(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              color: const Color(0xff171717)),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                onTap: () => Navigator.push(
+                SizedBox(height: 5.h),
+                BasicTextFormField(
+                  text: locale.id,
+                  isNumbers: true,
+                ),
+                BasicTextFormField(
+                  text: locale.password,
+                  isPass: true,
+                ),
+                SizedBox(height: 10.h),
+                InkWell(
+                  onLongPress: () => Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: route,
-                    )),
-                child: Container(
-                  height: 7.h,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(color: Color(0xff159BAD)),
-                  child: Center(
-                    child: Text(
-                      locale.login,
-                      style: GoogleFonts.cairo(
-                          textStyle: const TextStyle(
-                        color: Color(0xffFFFFFF),
-                        fontSize: 16,
+                      builder: (context) => const ClinicChoiceView(),
+                    ),
+                  ),
+                  onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: route,
                       )),
+                  child: Container(
+                    height: 7.h,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(color: Color(0xff159BAD)),
+                    child: Center(
+                      child: Text(
+                        locale.login,
+                        style: GoogleFonts.cairo(
+                            textStyle: const TextStyle(
+                          color: Color(0xffFFFFFF),
+                          fontSize: 16,
+                        )),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Spacer(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                  locale.noAccount,
-                    style: GoogleFonts.cairo(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: const Color(0xff000000),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const BookingIntroView(),
-                          ));
-                    },
-                    child: Text(
-                      locale.registerNow,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      locale.noAccount,
                       style: GoogleFonts.cairo(
                         fontSize: 14,
                         fontWeight: FontWeight.w400,
-                        color: const Color(0xff4DC143),
+                        color: const Color(0xff000000),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const BookingIntroView(),
+                            ));
+                      },
+                      child: Text(
+                        locale.registerNow,
+                        style: GoogleFonts.cairo(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: const Color(0xff4DC143),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
